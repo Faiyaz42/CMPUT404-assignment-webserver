d72f9cc8359229cddadf59798e9e0912d2eb60af
Faiyaz Ahmed <50289212+Faiyaz42@users.noreply.github.com>
2021-09-27 07:26:19 -0600

    Commits

--- README.md
+++ README.md
@@ -18,9 +18,16 @@ server.py contains contributions from:
 * Eddie Antonio Santos
 * Jackson Z Chang
 * Mandy Meindersma 
+* Faiyaz Ahmed
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
+
+Resources:
+
+1. https://bhch.github.io/posts/2017/11/writing-an-http-server-from-scratch/
+2. HTTP slides from CMPUT404 Eclass for http parsing
 
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
 import socketserver
+import os.path
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2021 Abram Hindle, Eddie Antonio Santos, Faiyaz Ahmed
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +18,7 @@ import socketserver
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright Â© 2001-2021 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -32,7 +33,84 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        root = "./www"
+        method, file, host = self.parse_request()  
+        
+        path = root + file  # path 
+        response = None
+        
+        if file.endswith("/"):
+            path = path + "index.html" #to homepage
+
+       
+        if method != "GET":                 #determine if its allowed method and not HEAD POST PUT DELETE  
+            response = self.response_405()     
+        elif not file.endswith("/") and os.path.isdir(path):     # if file exists and not "/", redirect
+            redirec_path = file + "/"       
+            response = self.response_301(redirec_path, host)
+        elif os.path.isfile(path):                         #if specified path exist
+            mime_type = self.get_mime_type(path)           #find file type or if folder
+            if mime_type != "folder":
+                content = self.file_content(path)
+                if content != "Error reading file!":
+                    response = self.response_200(mime_type, content)  #Ok return
+                else:
+                    response = self.response_404()        #errors
+            else:
+                response = self.response_404()
+        else:
+                response = self.response_404()
+
+        self.request.sendall(bytearray(response,'utf-8'))     #encoding and return response
+
+
+    
+    def parse_request(self):
+        host = None
+        lines = self.data.decode("utf-8").splitlines()  #decode data received to utf-8 from byte and create list of each line content
+        method, file, protocol = lines[0].split()      #take the first line and split its content into method, file and protocol
+        for line in lines:                             #find the host in the list of "lines"
+            if "Host:" in line:
+                host = "http://"+line.split()[-1]
+        return method, file, host
+
+            
+
+    def file_content(self, path):        #reading file and returning its content
+        try:
+            file = open(path, "r") 
+        except Exception:
+            content = "Error reading file!"
+        else:
+            content = file.read()
+            file.close()
+        finally:
+            return content
+
+
+   
+    def get_mime_type(self, path):
+        ext = os.path.splitext(path)[1]
+        if ext != "":
+            return "text/" + ext.split(".")[1] # for a valid file,there is extension
+        else:
+            return "folder"    # a folder,no extension
+
+            
+   
+    def response_200(self, mime_type, content):
+        return "HTTP/1.1 200 OK\r\nContent-Type: " + mime_type + "\r\n\r\n" + content + "\r\n"  #found content
+
+    def response_301(self, redirect_path, host):
+        return "HTTP/1.1 301 Moved Permanently\r\nLocation: " + host + redirect_path + "\r\n"  #redirect to redirect_path
+
+    def response_404(self):
+        return "HTTP/1.1 404 Not Found\r\nConnection: close\r\n" #connection close if not found
+
+    def response_405(self):
+        return "HTTP/1.1 405 Method Not Allowed\r\nConnection: close\r\n" #connection close if method not implemented
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
